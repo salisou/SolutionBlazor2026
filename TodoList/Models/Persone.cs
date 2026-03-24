f53c27b14e1d31d5376557bd1d9c62dd39fe41b9
Moussa <salisoumoussa1@gmail.com>
2026-03-24 13:07:57 +0100

    Added GestionePersone

---
+++ TodoList/Models/Persone.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Models
+{
+    public class Persone
+    {
+        public int Id {  get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cognome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateOnly DataNascita { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+}
